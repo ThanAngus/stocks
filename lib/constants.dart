cf294041046bae7fadc4c9d5d078674042e71ea6
ThanAngus <41669344+ThanAngus@users.noreply.github.com>
2022-11-03 21:13:33 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
-const String marketStackApi = '9a34f4f1610fef2f36a33dc924fb4506';
+const String marketStackApi = '$apiKey';
 const String apiUrl = "http://api.marketstack.com/v1/tickers?access_key=$marketStackApi&limit=10&count=1?q={'has_eod':'true'}";
 const String apiUrlEod = "http://api.marketstack.com/v1/eod?access_key=$marketStackApi&symbols=";
